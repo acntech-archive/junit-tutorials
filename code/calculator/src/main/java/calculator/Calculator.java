fadf59e03f2637c09b5c0c00a07a77839b6fe90b
Fredrik Bjørnøy <fredrik.bjornoy@MW7L21LJET7H0Z.dir.svc.accenture.com>
2014-04-30 13:33:10 +0100

    Created calculatorproject

---
+++ code/calculator/src/main/java/calculator/Calculator.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public class Calculator {
+
+    public int add(int augend, int addend) {
+        return augend + addend;
+    }
+
+    public int subtract(int minuend, int subtrahend) {
+        return minuend - subtrahend;
+    }
+
+    public int multiply(int multiplicand, int multiplier) {
+        return multiplicand * multiplier;
+    }
+
+
+    public int divide(int dividend, int divisor) {
+        return dividend / divisor;
+    }
+}
---
+++ code/calculator/src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,51 @@
+package calculator;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+
+public class CalculatorTest {
+
+    @Test
+    public void testSumShouldBeFiveWhenAddingTwoPlusThree() {
+        Calculator calculator = new Calculator();
+        int sum = calculator.add(2, 3);
+        assertThat("Wrong sum", sum, is(5));
+    }
+
+    @Test
+    public void testDifferenceShouldBe10WhenSubtractingTenFromTwenty() {
+        Calculator calculator = new Calculator();
+        int difference = calculator.subtract(20, 10);
+        assertThat("Wrong difference", difference, is(10));
+    }
+
+    @Test
+    public void testProductShouldBeHundredWhenMultiplyingTenAndTen() {
+        Calculator calculator = new Calculator();
+        int product = calculator.multiply(10, 10);
+        assertThat("Wrong product", product, is(100));
+    }
+
+    @Test
+    public void testQuotientShouldBeTwoWhenDividingTenAndFive() {
+        Calculator calculator = new Calculator();
+        int quotient = calculator.divide(10, 5);
+        assertThat("Wrong quotient", quotient, is(2));
+    }
+
+    @Test
+    public void testDividingByZeroIsIllegal() {
+        Calculator calculator = new Calculator();
+        try {
+            int quotient = calculator.divide(1, 0);
+            fail("This line should never be reached!");
+        } catch (Exception e) {
+            assertThat("Wrong exceptionmessage", e.getMessage(), is("/ by zero"));
+        }
+
+    }
+}
